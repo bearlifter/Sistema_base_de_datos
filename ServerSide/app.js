70536454b6c4157de4c0ddd0d57b21da9c415c72
bearlifter <tavauditore@gmail.com>
2024-08-22 15:05:15 -0700

    creé un form para poder añadir libro y autor

--- ServerSide/app.js
+++ ServerSide/app.js
@@ -6,16 +6,31 @@ const port = 3000;
 // Set EJS as the view engine
 app.set("view engine", "ejs");
 
+app.use(express.urlencoded({extended: true}));
+
+
 // Sample data
-const books = [
+let books = [
   { title: "The Great Gatsby", author: "F. Scott Fitzgerald" },
   { title: "To Kill a Mockingbird", author: "Harper Lee" },
   { title: "1984", author: "George Orwell" },
 ];
 
+
+
 // Route for the home page
 app.get("/", (req, res) => {
   res.render("index", { books: books });
+});
+
+app.post("/add-book", (req, res) => {
+
+  const {title, author} = req.body;
+
+  books.push({title: title, author: author});
+
+  res.redirect("/");
+
 });
 
 // Start the server
